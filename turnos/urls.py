8c0393f8fc0097156696a5bcc076425cb8263293
Rodrigo <rreyes2025@alu.uct.cl>
2026-05-10 12:36:09 -0400

    feat: implementa ReservaFacade y conecta backend con frontend para el MVP

--- turnos/urls.py
+++ turnos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # 👈 ESTA ES LA CLAVE
     path('agenda/', views.agenda, name='agenda'),
+    path('reservar/', views.confirmar_reserva, name='confirmar_reserva'),
 ]
