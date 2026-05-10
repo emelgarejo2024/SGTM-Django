cace1fc3928e9b78c74bd2a96cc9c051dbe06af4
Edgar Melgarejo <emelgarejo2024@alu.uct.cl>
2026-05-10 04:44:05 -0400

    Implementacion de factory method y base de Api interna

---
+++ turnos/factories.py
@@ -0,0 +1,30 @@
+# turnos/factories.py
+from .models import Usuario
+
+class UsuarioFactory:
+    """
+    Patrón Creacional: Factory Method.
+    Centraliza y estandariza la creación de usuarios según su rol.
+    """
+    @staticmethod
+    def crear_usuario(tipo_rol, rut, username, password, first_name='', last_name='', email=''):
+        # Validar que el rol exista en las opciones del modelo
+        roles_permitidos = dict(Usuario.ROLES).keys()
+        if tipo_rol not in roles_permitidos:
+            raise ValueError(f"El rol '{tipo_rol}' no es válido. Usa: {list(roles_permitidos)}")
+        
+        # Instanciar el usuario
+        nuevo_usuario = Usuario(
+            rol=tipo_rol,
+            rut=rut,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        
+        # En Django, la contraseña siempre debe hashearse con set_password()
+        nuevo_usuario.set_password(password)
+        nuevo_usuario.save()
+        
+        return nuevo_usuario
--- turnos/views.py
+++ turnos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Especialidad
 
-# Create your views here.
+def api_obtener_especialidades(request):
+    """
+    Cumple con el requisito de "Uso de APIs" de la rúbrica.
+    Devuelve todas las especialidades de la base de datos en formato JSON.
+    """
+    # Transformamos el QuerySet en una lista de diccionarios
+    especialidades_lista = list(Especialidad.objects.values('id', 'nombre'))
+    
+    # Retornamos el JSON. safe=False permite enviar listas u otros objetos no-diccionario en el root.
+    return JsonResponse({'especialidades': especialidades_lista}, safe=False)
