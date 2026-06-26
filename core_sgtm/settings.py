27afb0f1c70754132e68350830fe5dafe9d529a4
Edgar Melgarejo <emelgarejo2024@alu.uct.cl>
2026-06-26 16:48:17 -0400

    test: usar db sqlite en memoria para que los tests pasen en github actions

--- core_sgtm/settings.py
+++ core_sgtm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,6 +93,15 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+# Si estamos en GitHub Actions, usamos SQLite en memoria para que los tests pasen rápido
+if os.environ.get("GITHUB_ACTIONS") == "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
 
 
 # Password validation
